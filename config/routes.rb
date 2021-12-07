0f74cecf19ef14d40eaaf8601e4b784b03f12e9b
kn1025yu <sn22kzh@gmail.com>
2021-12-07 14:15:53 +0000

    [fix]READMEの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+      get :followings, :followers
+    end
+  end
+
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
+  resources :relationships, only: [:create, :destroy]
+
+  get '/search', to: 'searchs#search'
+
+  post '/posts/new', to: 'posts#create', as: :create
 end
