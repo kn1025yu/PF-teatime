d25d647f9149e3d99987f3e13a2e89dc343aa701
kn1025yu <sn22kzh@gmail.com>
2021-12-15 09:39:53 +0000

    [fix]問い合わせ機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
+  # Don't care if the mailer can't send.↓SMTPサーバを使えるように変更:false → true
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
@@ -59,6 +59,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  #Gmail送信用サーバの設定
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
