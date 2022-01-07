b0a65d21cc009e6c046fc8e80e5c4c7b6adfddcc
kn1025yu <sn22kzh@gmail.com>
2022-01-07 16:22:06 +0000

    [fix]検索機能の編集

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -17,20 +17,12 @@ class SearchsController < ApplicationController
        @user = User.where(name: content)
       elsif method == 'partial'
         User.where('name LIKE ?', "%#{content}%")
-      elsif method == 'pre'
-        User.where('name LIKE ?', "#{content}%")
-      elsif method == 'back'
-        User.where('name LIKE ?', "%#{content}")
       end
     elsif model == 'post_image'
       if method == 'perfect'
         PostImage.where(title: content)
       elsif method == 'partial'
         PostImage.where('title LIKE ?', "%#{content}%")
-      elsif method == 'pre'
-        PostImage.where('title LIKE ?', "#{content}%")
-      elsif method == 'back'
-        PostImage.where('title LIKE ?', "%#{content}")
       end
     end
   end
