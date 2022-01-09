241d28024847cdb93a3b4ab2b0079c2558082f41
kn1025yu <sn22kzh@gmail.com>
2022-01-09 13:52:16 +0000

    [fix]コントローラ編集

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,13 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
+  before_action post_image_params, only: [:create, :destroy]
 
   def create
-    @post_image = PostImage.find(params[:post_image_id])
     bookmark = @post_image.bookmarks.new(user_id: current_user.id)
     bookmark.save
   end
 
   def destroy
-    @post_image = PostImage.find(params[:post_image_id])
     bookmark = Bookmark.find_by(user_id: current_user.id, post_image_id: @post_image.id)
     bookmark.destroy
   end
