63a292be3ffbf202069ae1b6b09941ca9f906c60
kn1025yu <sn22kzh@gmail.com>
2022-01-13 14:36:26 +0000

    [fix]コントローラのエラー箇所編集

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,19 +1,16 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
-  before_action post_image_params, only: [:create, :destroy]
 
   def create
+    @post_image = PostImage.find(params[:post_image_id])
     bookmark = @post_image.bookmarks.new(user_id: current_user.id)
     bookmark.save
   end
 
   def destroy
+    @post_image = PostImage.find(params[:post_image_id])
     bookmark = Bookmark.find_by(user_id: current_user.id, post_image_id: @post_image.id)
     bookmark.destroy
   end
 
-  private
-  def post_image_params
-    @post_image = PostImage.find(params[:post_image_id])
-  end
 end
