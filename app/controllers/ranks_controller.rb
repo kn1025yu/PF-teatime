c9fd16f04cc5b64fa0abf98a56862a9d92798991
kn1025yu <sn22kzh@gmail.com>
2022-02-05 16:56:36 +0000

    [delete]不要なコントローラ等の削除

--- app/controllers/ranks_controller.rb
+++
@@ -1,6 +0,0 @@
-class RanksController < ApplicationController
-  def rank
-    # 投稿のいいね数ランキング
-    @post_image_bookmark_ranks = PostImage.find(Bookmark.group(:post_image_id).order('count(post_image_id) desc').pluck(:post_image_id))
-  end
-end
